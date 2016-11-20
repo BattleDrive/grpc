0b42d60a7a53cdf6ba38ade3439a80e3e21337e8
crazyproger <vladimir.rudev@gmail.com>
2016-11-21 00:17:12 +0300

    Comment

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# example run command: env OPENSSL_INCLUDE="/Users/crazy/projects/solutions/cars_client/UE4/Engine/Source/ThirdParty/OpenSSL/1.0.2h/include/" OPENSSL_LIB_MAC="/Users/crazy/projects/solutions/cars_client/UE4/Engine/Source/ThirdParty/OpenSSL/1.0.2h/lib/Mac" OPENSSL_LIB_IOS="/Users/crazy/projects/solutions/cars_client/UE4/Engine/Source/ThirdParty/OpenSSL/1.0.2h/lib/IOS" ./build.sh
+
 cd "`dirname "$0"`"
 export EMBED_OPENSSL=false
 if [ "$(uname)" = "Darwin" ]; then
