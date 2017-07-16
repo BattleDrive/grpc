b8c840a71db53d659e052f4506a2546f735c34d2
Sergey Pavlov <graypvlov@gmail.com>
2017-07-16 16:09:08 +0300

    Fix building for Windows

--- build.bat
+++ build.bat
@@ -6,17 +6,19 @@ pushd %~dp0
 if ERRORLEVEL 1 goto error
 
 cd third_party\protobuf\cmake
-cmake -G "Visual Studio 14 2015 Win64" -Dprotobuf_MSVC_STATIC_RUNTIME=OFF -Dprotobuf_BUILD_TESTS=OFF
+mkdir build & cd build
+mkdir solution & cd solution
+cmake -G "Visual Studio 14 2015 Win64" -Dprotobuf_MSVC_STATIC_RUNTIME=OFF -Dprotobuf_BUILD_TESTS=OFF ../..
 if ERRORLEVEL 1 goto error
 
 msbuild protobuf.sln /p:Platform=x64 /p:Configuration=Release
 if ERRORLEVEL 1 goto error
 
-cd ../../../vsprojects
-msbuild grpc.sln /p:Platform=x64 /p:Configuration=Release
+cd ../../../../../vsprojects
+msbuild grpc.sln /t:grpc /t:grpc++ /p:Platform=x64 /p:Configuration=Release
 if ERRORLEVEL 1 goto error
 
-msbuild grpc_protoc_plugins.sln /p:Platform=x64 /p:Configuration=Release
+msbuild grpc_protoc_plugins.sln /t:grpc_cpp_plugin /p:Platform=x64 /p:Configuration=Release
 if ERRORLEVEL 1 goto error
 
 rem Done
